e84eb079319c6852403965aaf9b7ed2a9a5dc797
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-13 16:15:36 -0400

    refactor(oauth/controllers): Replace hard-coded google auth implementation with a GoogleAuth instance; express-session is used to store google state secret

--- oauth/src/controllers/customAuthController.ts
+++ oauth/src/controllers/customAuthController.ts
@@ -1,33 +1,23 @@
 import { Request, Response } from "express";
-import { URL } from "node:url";
 
-import {
-  LOGGED_IN_REACT_ADDRESS,
-  GOOGLE_OAUTH_SCOPES,
-  googleAuthClient,
-  oauth2,
-  googleState,
-} from "../config";
+import { randomBytes } from "node:crypto";
+import { LOGGED_IN_REACT_ADDRESS } from "../config";
+import { googleAuth } from "../services/GoogleAuth";
 import { generateToken } from "../utils/generateToken";
+import { GSession } from "../types";
 
 let users: string[] = [];
 
+const newState = (): string => {
+  return randomBytes(32).toString("hex");
+};
+
 export const googleSignIn = async (req: Request, res: Response) => {
   try {
-    const state = googleState.setState();
-    const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
+    const state = newState();
+    (req.session as GSession).googleAuthState = state;
     // Generate a url that asks permissions for the Drive activity and Google Calendar scope
-    const authorizationUrl = googleAuthClient.generateAuthUrl({
-      // 'online' (default) or 'offline' (gets refresh_token)
-      access_type: "offline",
-      /** Pass in the scopes array defined above.
-       * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
-      scope: scopes,
-      // Enable incremental authorization. Recommended as a best practice.
-      include_granted_scopes: true,
-      // Include the state parameter to reduce the risk of CSRF attacks.
-      state: state,
-    });
+    const authorizationUrl = googleAuth.generateAuthUrl(state);
     // Redirect the user to authorizationUrl
     res.redirect(authorizationUrl);
   } catch (error) {
@@ -38,43 +28,21 @@ export const googleSignIn = async (req: Request, res: Response) => {
 
 export const googleCallback = async (req: Request, res: Response) => {
   console.info("googleCallback");
-  // We exchange the authorization code for an access and ID token
-  // by making a post request to Google’s access token endpoint
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  let q = Object.fromEntries(url.searchParams.entries());
-  if (q.error) {
-    // An error response e.g. error=access_denied
-    console.error("Error:" + q.error);
-    res.end("Error:" + q.error);
-    // Check state value
-  } else if (!googleState.checkState(q.state)) {
-    console.error("State mismatch. Possible CSRF attack");
-    res.end("State mismatch. Possible CSRF attack");
-  }
+  try {
+    const authenticated = await googleAuth.authenticate(
+      req.url,
+      req.headers,
+      (req.session as GSession).googleAuthState
+    );
+    console.log(authenticated);
+    if (authenticated) {
+      const { email, name } = authenticated;
+      // =======================================
+      // Database interaction
+      // =======================================
+      email && interactWithDatabase(email);
+      // =======================================
 
-  let { tokens } = await googleAuthClient.getToken(q.code);
-
-  // setCredentials(tokens) only stores the tokens in memory.
-  // If your server restarts, they will be lost unless you persisted
-  googleAuthClient.setCredentials(tokens);
-
-  if (
-    tokens.scope &&
-    tokens.scope.includes(GOOGLE_OAUTH_SCOPES[0]) &&
-    tokens.scope.includes(GOOGLE_OAUTH_SCOPES[1])
-  ) {
-    // Because we are communicating directly with a Google server,
-    // We can be confident that the token is valid
-    const { data } = await oauth2.userinfo.get();
-    const { email, name } = data;
-
-    // get user or create user
-    let user = email && users.includes(email);
-    if (email && !user) {
-      users.push(email);
-      console.log("users email array: ", users);
-    }
-    try {
       const token = email && name && generateToken({ email, name });
       res.cookie("token", token, {
         httpOnly: true,
@@ -83,12 +51,18 @@ export const googleCallback = async (req: Request, res: Response) => {
       });
       res.redirect(LOGGED_IN_REACT_ADDRESS);
       return;
-    } catch (error) {
-      console.error("Error generating token:", error);
-      res.status(500).json({ message: "Failed to generate token" });
-      return;
-    } finally {
-      googleState.clearState();
     }
+  } catch (error) {
+    res.status(500).json({ message: "Google authentication error: ", error });
+    return;
+  } finally {
+    (req.session as GSession).googleAuthState = "";
+  }
+};
+
+const interactWithDatabase = (email: string) => {
+  let user = email && users.includes(email);
+  if (email && !user) {
+    users.push(email);
   }
 };
