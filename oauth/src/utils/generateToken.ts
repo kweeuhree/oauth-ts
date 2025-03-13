c7b26eea57e97ec7cfdf5ef403623332afd96614
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-13 09:49:47 -0400

    feat(utils): Move generateToken into utils folder

---
+++ oauth/src/utils/generateToken.ts
@@ -0,0 +1,25 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+import { GOOGLE_CLIENT_ID } from "../config";
+
+export const generateToken = ({
+  email,
+  name,
+}: {
+  email: string;
+  name: string;
+}) => {
+  const options: SignOptions = {
+    algorithm: "HS256",
+    expiresIn: "1d",
+    issuer: "AIHelper",
+    audience: GOOGLE_CLIENT_ID,
+  };
+  console.log(options);
+  const token = jwt.sign(
+    { id: email, username: name },
+    String(process.env.JWT_SECRET),
+    options
+  );
+  return token;
+};
