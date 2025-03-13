efa4e9749dd495ebbb2f3bf964ac336f8a4328b7
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-13 18:06:30 -0400

    feat(oauth/types): Add types declaration and update tsconfig to include it

---
+++ oauth/src/types.d.ts
@@ -0,0 +1,6 @@
+// this type declaration extends default express-session types
+import { Session } from "express-session";
+
+export interface GSession extends Session {
+  googleAuthState: string;
+}
