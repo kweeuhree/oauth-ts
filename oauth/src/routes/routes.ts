518791ea8ef965603fce1109e949afbc28004e0f
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-14 13:14:04 -0400

    fix(oauth/routes): Remove logic related to Auth0

--- oauth/src/routes/routes.ts
+++ oauth/src/routes/routes.ts
@@ -1,17 +1,7 @@
 import express from "express";
-import { requiresAuth } from "express-openid-connect";
-
-import { authMiddleware } from "../middleware/authMiddleware";
-import {
-  customAuthController,
-  authController,
-  userController,
-} from "../controllers/index";
+import { customAuthController } from "../controllers/index.js";
 
 export const router = express.Router();
-
-// "login" is reserved by Auth0 <------
-// router.post("/login", authController.login);
 
 // Google OAuth
 router.get("/auth/google", customAuthController.googleSignIn);
@@ -20,7 +10,3 @@ router.get("/google-callback", customAuthController.googleCallback);
 // GitHub OAuth
 router.get("/auth/github", customAuthController.githubSignIn);
 router.get("/github-callback", customAuthController.githubCallback);
-
-// Auth0
-router.post("/api/callback", authController.auth0callback);
-router.get("/getter", authMiddleware, userController.get);
