8d6d36cf78bca03acc78bd304a5bae9f6bfc9e4d
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-14 13:07:32 -0400

    update(oauth/githubConfig): Add GitHub token info url and github redirect url

--- oauth/src/config/githubConfig.ts
+++ oauth/src/config/githubConfig.ts
@@ -1,2 +1,5 @@
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+
+export const GITHUB_TOKEN_INFO_URL = process.env.GITHUB_TOKEN_INFO_URL;
+export const GITHUB_REDIRECT_URL = `https://github.com/login/oauth/authorize?`;
